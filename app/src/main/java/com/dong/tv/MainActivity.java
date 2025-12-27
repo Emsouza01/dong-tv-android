e4d13ed081f566a5b457075fa0ffafcd2e3d4005
Emsouza01 <63312236+Emsouza01@users.noreply.github.com>
2025-12-27 17:52:08 -0300

    Update MainActivity.java

--- app/src/main/java/com/dong/tv/MainActivity.java
+++ app/src/main/java/com/dong/tv/MainActivity.java
@@ -1,1 +1,38 @@
+package com.dong.tv;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webview);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setMediaPlaybackRequiresUserGesture(false);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://emsouza01.github.io/dong-app/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        WebView webView = findViewById(R.id.webview);
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
 
