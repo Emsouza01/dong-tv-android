ddab21cb5a5e74e392b9879e029485510e296be2
Emsouza01 <63312236+Emsouza01@users.noreply.github.com>
2025-12-27 18:10:47 -0300

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,1 +1,11 @@
+@echo off
+setlocal
+
+set DIRNAME=%~dp0
+set APP_HOME=%DIRNAME%
+
+set JAVA_EXE=java.exe
+if defined JAVA_HOME set JAVA_EXE=%JAVA_HOME%\bin\java.exe
+
+"%JAVA_EXE%" -classpath "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
 
